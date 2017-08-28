b5bc6e24cdc908b355aa75c3fabc84bae40d1d5b
Douglas Henrique <lima.dougglas@gmail.com>
2017-08-28 15:50:40 -0300

    interface e funções otimizadas

--- js/script.js
+++ js/script.js
@@ -1,52 +1,83 @@
-$("#calcular").click(function() {
-    alert("oi");
-    var a = -1,
-        b = 0,
-        c = 0,
-        cAux = 0,
-        numLinhas = 1;;
-
-    while (true) {
-
-        cAux = c;
-        c = (a + b) / 2;
-
-        // condição de parada
-        if ((Math.abs(cAux - c)) < 0.00001 || numLinhas > 10) {
-
-            break;
-        } else {
-            addLinhas(numLinhas++, a, b, c, funcao(a), funcao(b), funcao(c));
-        }
-
-        if ((funcao(b) >= 0 && funcao(c) >= 0) || (funcao(b) < 0 && funcao(c) < 0)) {
-            b = c;
-        } else {
-            a = c;
-        }
-    }
-
-    //  1=1 ->c sobrescreve b
-    // -1=1->c sobrescreve a
+// evento click botão
+$('#calcular').click(function() {
+    limparTabela();
+    dicotomia();
 });
 
 
+// evento enter edittext
+$('#fun').on('keypress', function(e) {
+    if (e.which === 13) {
+        limparTabela();
+        dicotomia();
 
-// calculando função.
-function funcao(x) {
-    var result = String($("#cal-func").val());
-    var result = nerdamer(result);
-    console.log(result);
-    // alert("oi2");
-    alert("fim");
-    return 1;
+        // desabilitando caixa de texto
+        $(this).attr("disabled", "disabled");
+        $(this).removeAttr("disabled");
+    }
+
+});
+
+
+// funcão gerencia dicotomia
+function dicotomia() {
+    var a = -100,
+        b = 0,
+        c = 0,
+        cAux = 0,
+        numLinhas = 1;
+
+    for (; a < 100; a++, b++) {
+
+        while (true) {
+
+            c = (a + b) / 2;
+
+            // condição de parada
+            if ((Math.abs(cAux - c)) < 0.000001 || numLinhas > 7900) {
+                addLinhas(numLinhas++, a, b, c, calDicotomia(a), calDicotomia(b), calDicotomia(c), true);
+                break;
+            } else {
+                addLinhas(numLinhas++, a, b, c, calDicotomia(a), calDicotomia(b), calDicotomia(c), false);
+            }
+
+            if ((calDicotomia(b) >= 0 && calDicotomia(c) >= 0) || (calDicotomia(b) < 0 && calDicotomia(c) < 0)) {
+                b = c;
+            } else {
+                a = c;
+            }
+
+            cAux = c;
+        }
+
+    }
+
+}
+
+
+// calculo principal
+function calDicotomia(x) {
+
+    // tratando a função, substituindo as variáveis por número. 
+    funcao = String($('#fun').val());
+    funcao = funcao.replace(/[a-z]/ig, String(x));
+
+    // calculando a função tratada.
+    var result = nerdamer(funcao).evaluate();
+
+    // log
+    console.log("funcao: " + funcao);
+    console.log("result: " + result);
+
+    return parseFloat(result.text());
+
 }
 
 
 
 // adicionando linha na tabela.
-function addLinhas(numLinhas, a, b, c, fa, fb, fc) {
-    var newRow = $("<tr>");
+function addLinhas(numLinhas, a, b, c, fa, fb, fc, sucesso) {
+    var newRow = (sucesso) ? $("<tr class='table-success'>") : $("<tr>");
     var cols = "";
     cols += '<th scope="row">' + numLinhas + '</th>';
     cols += '<td>' + a + '</td>';
@@ -59,5 +90,11 @@ function addLinhas(numLinhas, a, b, c, fa, fb, fc) {
     newRow.append(cols);
 
     $("#tabela-dicotomia").append(newRow);
+}
 
+
+// removendo todas as linhas da tabela.
+function limparTabela() {
+    var linhas = $('#tabela-dicotomia tbody tr'); //tr
+    linhas.remove();
 }
